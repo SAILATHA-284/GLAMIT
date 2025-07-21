813a4a5d28a705126b2704290e41015b909c4aa8
SAILATHA-284 <sai.she.rt22@dypatil.edu>
2025-07-22 01:16:50 +0530

    Update firebase.js

--- GlamIt/src/firebase/firebase.js
+++ GlamIt/src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCwmhEZtgkXzABXxA7pWy6bXujL2hLfOf4",
-	authDomain: "insta-clone-yt-1e993.firebaseapp.com",
-	projectId: "insta-clone-yt-1e993",
-	storageBucket: "insta-clone-yt-1e993.appspot.com",
-	messagingSenderId: "609993461712",
-	appId: "1:609993461712:web:7b25eccedd40bf4a2e33d5",
-	measurementId: "G-2YT1SXREM9"
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket:  process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId:  process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+	appId:  process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 
 const app = initializeApp(firebaseConfig);
